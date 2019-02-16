0d02ffaa79caabbf5a8084394bcc471f2518918c
iYung <ivankhyung@gmail.com>
2019-02-16 16:00:09 -0500

    created route for getting alarms

--- server.js
+++ server.js
@@ -4,47 +4,67 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+if (process.env.ENVIRO != "PROD") require('dotenv').config();
+
 var port = process.env.PORT || 1337;
-
-// Create connection to database
-var config =
-{
-    userName: process.env.user, // update me
-    password: process.env.pass, // update me
-    server: process.env.db, // update me
-    options:
-    {
-        database: 'your_database', //update me
-        encrypt: true
-    }
-}
-
-var connection = new Connection(config);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
   console.log('Req made');
   next();
 });
 
 app.use(express.static('public'));
 
-//get lastest replays, new replay
 app.route('/alarms')
     .get(function(req, res) {
         var request = new Request(
             "SELECT * FROM Alarms",
             function(err, rowCount, rows)
             {
-                if (err) return res.send(err);
-                return res.send(rows);
-                process.exit();
+                console.log(rows);
             }
         );
+        request.on('doneInProc', function (rowCount, more, rows) {
+            var jsonArray = [];
+            rows.forEach(function (columns) {
+                var rowObject ={};
+                columns.forEach(function(column) {
+                    rowObject[column.metadata.colName] = column.value;
+                });
+                jsonArray.push(rowObject);
+            });
+            return res.json(jsonArray);
+        });
+        connect(request);
     });
 
 app.listen(port);
+
+function connect(request) {
+    var config =
+    {
+        userName: process.env.user, // update me
+        password: process.env.pass, // update me
+        server: process.env.db, // update me
+        options:
+        {
+            database: process.env.dbname, //update me
+            encrypt: true,
+            rowCollectionOnDone: true
+        }
+    }
+    var connection = new Connection(config);
+    connection.on('connect', function(err) {
+        if (err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            connection.execSql(request);
+        }
+    });
+}
