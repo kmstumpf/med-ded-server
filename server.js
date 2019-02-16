d6f1622abd52d324f8c35d0da289a50bfa8527d9
iYung <ivankhyung@gmail.com>
2019-02-16 12:59:42 -0500

    startinging server

---
+++ server.js
@@ -0,0 +1,45 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 1337;
+
+// Create connection to database
+var config =
+{
+    userName: 'your_username', // update me
+    password: 'your_password', // update me
+    server: 'your_server.database.windows.net', // update me
+    options:
+    {
+        database: 'your_database', //update me
+        encrypt: true
+    }
+}
+//var connection = new Connection(config);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  console.log('Req made');
+  next();
+});
+
+//get lastest replays, new replay
+app.route('/')
+    .get(function(req, res) {
+        return res.send("Hi");
+    });
+
+//get lastest replays, new replay
+app.route('/alarms')
+    .get(function(req, res) {
+        return res.send("Hi");
+    });
+
+app.listen(port);
